5efb72d6cb80d7eb8ecd4440aef8aa0744014b6d
Quintaneishon <ajitzi.quintana@gmail.com>
2020-05-17 19:13:57 -0500

    Updates

---
+++ precache-manifest.0dad8988a5599889afdeca1dda733ac4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca0407d4764c85c8b785006843a065ae",
+    "url": "/personal-page/index.html"
+  },
+  {
+    "revision": "2537de6489e18e734c25",
+    "url": "/personal-page/static/css/main.ff00138c.chunk.css"
+  },
+  {
+    "revision": "308837dcc443ca5feead",
+    "url": "/personal-page/static/js/2.28c265e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/personal-page/static/js/2.28c265e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2537de6489e18e734c25",
+    "url": "/personal-page/static/js/main.dccd82bf.chunk.js"
+  },
+  {
+    "revision": "b275daa608167b8e384c",
+    "url": "/personal-page/static/js/runtime-main.efb85169.js"
+  }
+]);
